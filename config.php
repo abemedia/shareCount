b13e357d72689476f2a6b28b6f0123c4ec0539b0
abeMedia <info@abemedia.co.uk>
2013-12-16 14:43:45 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 class config {
-	public $use_cache = true;
-	public $cache_directory = './cache/';
-	public $cache_time = "86400"; // in seconds; 86400 seconds = 24h
+	public static $use_cache = true;
+	public static $cache_directory = './cache/';
+	public static $cache_time = "86400"; // in seconds; 86400 seconds = 24h
 	
 	// default settings - ignored if url parameter has been passed
-	public $format = "json"; // "json", "jsonp" or "xml"
-	public $callback = "processShares"; // default 
+	public static $format = "json"; // "json", "jsonp" or "xml"
+	public static $callback = "processShares"; // default 
 	
 }
