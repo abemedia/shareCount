670241578d62cfc86cbe2ef667f5eccad7f7eca3
abeMedia <info@abemedia.co.uk>
2013-12-16 13:45:38 +0000

    Update clean_cache.php

--- clean_cache.php
+++ clean_cache.php
@@ -1,15 +1,17 @@
 <?php
 require("config.php");
-
-    if ($handle = @opendir($config->cache_directory)) {
-        while (false !== ($file = @readdir($handle))) {
-            if ($file != '.' and $file != '..') {
-				$file_created = ((@file_exists($file))) ? @filemtime($file) : 0;
-				if (time() - config::cache_time < $file_created) {
-					echo $file . ' deleted.<br>';
-					@unlink(config::cache_directory . '/' . $file);
-				}
-            }
-        }
-        @closedir($handle);
-    }
+$i = 0;
+if ($handle = @opendir($config->cache_directory)) {
+	while (false !== ($file = @readdir($handle))) {
+		if ($file != '.' and $file != '..') {
+			$file_created = ((@file_exists($file))) ? @filemtime($file) : 0;
+			if (time() - config::cache_time < $file_created) {
+				$i++;
+				echo $file . ' deleted.<br>';
+				@unlink(config::cache_directory . '/' . $file);
+			}
+		}
+	}
+	@closedir($handle);
+}
+if($i==0) echo 'Nothing to clean.';
