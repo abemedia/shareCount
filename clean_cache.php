a4a33b20398aa1dfd0de01e6d676c1c31ba97d87
abeMedia <info@abemedia.co.uk>
2013-12-16 14:29:01 +0000

    Update clean_cache.php

--- clean_cache.php
+++ clean_cache.php
@@ -1,17 +1,5 @@
 <?php
-require("config.php");
-$i = 0;
-if ($handle = @opendir($config->cache_directory)) {
-	while (false !== ($file = @readdir($handle))) {
-		if ($file != '.' and $file != '..') {
-			$file_created = ((@file_exists($file))) ? @filemtime($file) : 0;
-			if (time() - config::cache_time < $file_created) {
-				$i++;
-				echo $file . ' deleted.<br>';
-				@unlink(config::cache_directory . '/' . $file);
-			}
-		}
-	}
-	@closedir($handle);
-}
-if($i==0) echo 'Nothing to clean.';
+require("getcount.php");
+$count = new shareCount;
+$count->cleanCache();
+?>
