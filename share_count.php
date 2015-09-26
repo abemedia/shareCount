2c41ef324dd9598df7a0433d8a5f0fd9a7a63796
Davide Melle <daneel87@gmail.com>
2015-09-26 10:48:18 +0200

    set CURLOPT_TIMEOUT

--- share_count.php
+++ share_count.php
@@ -115,6 +115,7 @@ class shareCount {
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 1);
             $data = curl_exec($ch);
             curl_close($ch);
         }
